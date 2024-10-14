38cf744b2d36e0a8a8246471714fa1e567cdda2a
jvdragon05 <joaovitortbt@gmail.com>
2024-10-13 22:59:33 -0300

    Banco Atualizado

--- sql/slearn.sql
+++ sql/slearn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 14/10/2024 às 03:04
+-- Tempo de geração: 14/10/2024 às 03:58
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
